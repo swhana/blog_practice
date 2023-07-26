5e4542d2720d42a2b2c438bc4641a34f21d78e87
swhana <swhan0428@gmail.com>
2023-07-26 19:23:24 +0900

    Add route feature

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,33 +1,20 @@
 //Koa framework로 백엔드 서버 띄우기
 const Koa = require('koa');
+const Router = require('koa-router');
 
 const app = new Koa();
+const router = new Router();
 
-//실제 서버에 띄워짐
-app.use((ctx, next) => {
-  console.log(ctx.url);
-  console.log("1번째 미들웨어 실행");
-  //요청 경로의 authorized 쿼리 파라미터가 1이 아니면 return
-  if (ctx.query.authorized !== '1') {
-    ctx.status = 401;
-    return;
-  }
-  next().then(() => {
-    console.log("END 1");
-  }) //next가 없으면 이 다음 미들웨어를 실행하지 않음
+router.get('/', ctx => {
+  ctx.body = '홈';
 });
 
-app.use(async (ctx, next) => {
-  console.log("2번째 미들웨어 실행");
-  await next();
-  console.log("END 2");
+router.get('/about', ctx => {
+  ctx.body = '소개';
 });
 
-app.use(ctx => {
-  ctx.body = "Hello World";
-});
+app.use(router.routes()).use(router.allowedMethods());
 
-//콘솔창에 띄워짐
 app.listen(4000, () => {
-  console.log("Listening to port 4000");
+  console.log('Listening to port 4000');
 });
