43fc4e3f2d2a3b75caecea027f44b6d888ae95bc
swhana <swhan0428@gmail.com>
2023-07-31 00:00:33 +0900

    Add REST api router

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,23 +2,15 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 
+const api = require('./api');
+
 const app = new Koa();
 const router = new Router();
 
-router.get('/', ctx => {
-  ctx.body = '홈';
-});
+//라우터 설정
+router.use('/api', api.routes());
 
-router.get('/about/:name?', ctx => {
-  const { name } = ctx.params; // /about/10 이런 식의 파라미터를 읽어옴
-  ctx.body = name ? `${name}의 소개 페이지` : '소개';
-});
-
-router.get('/posts', ctx => {
-  const { id } = ctx.query; // /posts/?id=10 이런 식의 쿼리를 읽어옴
-  ctx.body = id ? `${id}의 포스트` : '포스트 아이디가 없습니다';
-});
-
+//app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
