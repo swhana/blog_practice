cdc5a6d6be8925c7091954ea168dfc198899999d
swhana <swhan0428@gmail.com>
2023-08-04 13:30:29 +0900

    Add route feature

--- backend/src/api/posts/index.js
+++ backend/src/api/posts/index.js
@@ -1,4 +1,6 @@
 const Router = require('koa-router');
+const postCtrl = require('./posts.ctrl');
+
 const posts = new Router();
 
 const printInfo = ctx => {
@@ -9,12 +11,12 @@ const printInfo = ctx => {
   };
 };
 
-posts.get('/', printInfo);
-posts.post('/', printInfo);
-posts.post('/:name', printInfo);
-posts.get('/:id', printInfo);
-posts.delete('/:id', printInfo);
-posts.put('/:id', printInfo);
-posts.patch('/:id', printInfo);
+posts.get('/', postCtrl.list); //조회 기능
+posts.post('/', postCtrl.write); //기본 작성기능
+// posts.post('/:name', postCtrl.read);
+posts.get('/:id', postCtrl.read); //읽기 기능
+posts.delete('/:id', postCtrl.remove); //삭제 기능
+posts.put('/:id', postCtrl.replace); //전체 객체 업데이트
+posts.patch('/:id', postCtrl.update); //특정 객체 업데이트
 module.exports = posts;
 
