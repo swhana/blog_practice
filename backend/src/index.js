29303756b48dac330aa67d3defc9b592de6a4d7a
swhana <swhan0428@gmail.com>
2023-08-05 22:54:16 +0900

    Add mongoDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,18 @@
 //Koa framework로 백엔드 서버 띄우기
+require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const BodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
+
+const { PORT, MONGO_URI } = process.env;
+mongoose.connect(MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(e => {
+    console.log(e);
+  });
 
 const api = require('./api');
 
@@ -17,6 +28,8 @@ app.use(BodyParser());
 //app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('Listening to port 4000');
+const port = PORT || 4000;
+
+app.listen(port, () => {
+  console.log('Listening to port %d', port);
 });
